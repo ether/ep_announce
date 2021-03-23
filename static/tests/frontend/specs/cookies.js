332385072efe19d116a0b2e42db23a69d685fcb9
Richard Hansen <rhansen@rhansen.org>
2021-03-23 18:07:44 -0400

    Lint fixes

--- static/tests/frontend/specs/cookies.js
+++ static/tests/frontend/specs/cookies.js
@@ -1,11 +1,13 @@
+'use strict';
+
 describe('set audio announcement via cookie', function () {
   context('cookie is set to false', function () {
-    before(function (done) {
-      helper.newPad({
-        padPrefs: {'ep_announce-enabled': false},
-        cb: done,
-      });
+    before(async function () {
       this.timeout(60000);
+      await new Promise((resolve, reject) => helper.newPad({
+        padPrefs: {'ep_announce-enabled': false},
+        cb: (err) => err != null ? reject(err) : resolve(),
+      }));
     });
 
     it('the checkbox is set to false and will change to true', function (done) {
@@ -16,7 +18,8 @@ describe('set audio announcement via cookie', function () {
       expect($enableAnnounce.prop('checked')).to.be(false);
       $enableAnnounce.click();
       helper.waitFor(() => $enableAnnounce.prop('checked') === true, 1000).done(() => {
-        expect(chrome$.window.document.cookie.search('ep_announce-enabled%22%3Atrue')).to.not.be(-1);
+        expect(chrome$.window.document.cookie.search('ep_announce-enabled%22%3Atrue'))
+            .to.not.be(-1);
         expect(chrome$.window.document.cookie.search('ep_announce-enabled%22%3Afalse')).to.be(-1);
         done();
       });
@@ -24,12 +27,12 @@ describe('set audio announcement via cookie', function () {
   });
 
   context('cookie is set to true', function () {
-    before(function (done) {
-      helper.newPad({
-        padPrefs: {'ep_announce-enabled': true},
-        cb: done,
-      });
+    before(async function () {
       this.timeout(60000);
+      await new Promise((resolve, reject) => helper.newPad({
+        padPrefs: {'ep_announce-enabled': true},
+        cb: (err) => err != null ? reject(err) : resolve(),
+      }));
     });
 
     it('the checkbox is set to true and will change to false', function (done) {
@@ -39,7 +42,8 @@ describe('set audio announcement via cookie', function () {
       expect($enableAnnounce.prop('checked')).to.be(true);
       $enableAnnounce.click();
       helper.waitFor(() => $enableAnnounce.prop('checked') === false, 1000).done(() => {
-        expect(chrome$.window.document.cookie.search('ep_announce-enabled%22%3Afalse')).to.not.be(-1);
+        expect(chrome$.window.document.cookie.search('ep_announce-enabled%22%3Afalse'))
+            .to.not.be(-1);
         expect(chrome$.window.document.cookie.search('ep_announce-enabled%22%3Atrue')).to.be(-1);
         done();
       });
